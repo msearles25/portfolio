d11645c101b3da37d74f05464cda2362137eae2c
Michael <skaman4459@gmail.com>
2020-08-11 12:05:16 -0400

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,12 +8,27 @@ import Card from './card';
 import curio from '../images/curio.png'
 import hackathon from '../images/hackathonPortal.png'
 import ff from '../images/friendfinder.png'
+import facebrowser from '../images/facebrowser.png'
 
 const Projects = () => {
     return (
         <section id='projects' className='projectsSection'>
             <h3>Projects</h3>
             <div className='projectContainer'>
+            <ProjectCard 
+                    name='FaceBrowser Front-End' 
+                    image={facebrowser} 
+                    stack={['React', 'Styled-Components', 'Redux']}
+                    demo='https://face-browser.netlify.app/'
+                    source='https://github.com/msearles25/face-browser-frontend'
+                />
+                 <ProjectCard 
+                    name='FaceBrowser Back-End' 
+                    image={facebrowser} 
+                    stack={['Nodejs', 'Express']}
+                    demo='https://face-browser.netlify.app/'
+                    source='https://github.com/msearles25/face-browser-backend'
+                />
                 <ProjectCard 
                     name='Hackathon Portal' 
                     image={hackathon} 
